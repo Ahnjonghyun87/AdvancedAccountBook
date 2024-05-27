d720ab9e4294f7158db36a0c5f75e1ed4d987eb9
[Github <helgo1318@gmail.com>
2024-05-27 20:48:38 +0900

    feat:home.jsx에서 familyContext 상위호환만들고 props제거 및 props를 useContext hook 전환

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,31 @@
+import { useContext } from "react";
+import { FamilyContext } from "../../Context/FamilyContext";
 import CalendarForm from "../../components/CalendarForm";
 import RecentAccount from "../../components/RecentAccount";
 import WriteForm from "../../components/WriteForm";
 
-const Home = ({ selectedMonth, setSelectedMonth, posts, setPosts }) => {
+const Home = () => {
+  const { selectedMonth, setSelectedMonth, posts, setPosts } =
+    useContext(FamilyContext);
   const filteredPosts = posts.filter(({ date }) => {
     const month = new Date(date).getMonth() + 1;
     return month === selectedMonth;
   });
 
-  // 필터가 리턴하는 값은 무엇일까요?
-  // 1. 문자열 2.배열 3. 객체
-
-  //posts가 객체일까요? 배열일까요?
-  // 배열
-
-  // date 문자열?
-  // "2024-05-25"
-
   return (
-    <main>
-      <WriteForm setPosts={setPosts} post={posts} />
-      <CalendarForm
-        selectedMonth={selectedMonth}
-        setSelectedMonth={setSelectedMonth}
-      />
-      <RecentAccount posts={filteredPosts} />
-    </main>
+    <FamilyContext.Provider
+      value={{
+        setPosts: setPosts,
+        posts: posts,
+        selectedMonth: selectedMonth,
+        setSelectedMonth: setSelectedMonth,
+        filteredPosts: filteredPosts,
+      }}
+    >
+      <WriteForm />
+      <CalendarForm />
+      <RecentAccount />
+    </FamilyContext.Provider>
   );
 };
 
