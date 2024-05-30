6159e470cbac8c7612b9fdb518ff61881cf0a810
[Github <helgo1318@gmail.com>
2024-05-30 17:18:38 +0900

    feat app.jax-GlobalStyle수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import GlobalStyles from "./globalStyles";
+import GlobalStyles from "./GlobalStyles";
 import Detail from "./pages/Detail/Detail";
 import Home from "./pages/Home/Home";
 
 const App = () => {
-  const posts = useSelector((state) => state.posts.posts);
-  const selectedMonth = useSelector(
-    (state) => state.selectedMonth.selectedMonth
-  );
-
-  console.log("🚀 ~ App ~ selectedMonth:", selectedMonth);
-
-  useEffect(() => {
-    localStorage.setItem("posts", JSON.stringify(posts));
-  }, [posts]);
-
-  useEffect(() => {
-    localStorage.setItem("selectedMonth", JSON.stringify(selectedMonth));
-  }, [selectedMonth]);
-
   return (
     <>
       <GlobalStyles />
