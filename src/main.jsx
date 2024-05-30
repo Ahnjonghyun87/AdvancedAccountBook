27229487d5c6a5cf02014975be09619f6ed20589
[Github <helgo1318@gmail.com>
2024-05-30 19:36:42 +0900

    feat : GlobalStyle이 main.jsx에 있었던 오류 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import GlobalStyles from "./globalStyles";
+
 import store from "./redux/cofig/configStore";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyles />
       <App />
     </Provider>
   </React.StrictMode>
