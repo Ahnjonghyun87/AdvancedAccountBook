10b43d8de26b3fdcd76a7e209ffdd7e3137155ef
[Github <helgo1318@gmail.com>
2024-06-12 20:54:45 +0900

    feat:회원가입 로직 수정 유효성 검사 확인

--- src/pages/Login/SignInPage.jsx
+++ src/pages/Login/SignInPage.jsx
@@ -1,21 +1,41 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const SignInPage = () => {
   const navigate = useNavigate();
+
+  const [logInID, setLogInId] = useState("");
+  const [password, setPassWord] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [newUser, setNewUser] = useState([]);
+
+  const onAddUser = () => {
+    if (logInID.length < 4 || logInID.length > 10) {
+      alert("아이디는 4~10글자여야 합니다.");
+      return;
+    } else if (password.length < 4 || password.length > 15) {
+      alert("비밀번호는 4~15글자여야 합니다.");
+      return;
+    } else if (nickName.length < 1 || nickName.length > 10) {
+      alert("닉네임은 1~10글자여야 합니다.");
+      return;
+    } else {
+      alert("회원가입이 완료되었습니다!");
+      navigate("/home");
+    }
+
+    const newUser = ({ id, password, nickName }) => {
+      setNewUser((prev) => {
+        [...prev, newUser];
+      });
+    };
+  };
+
   const onSubmit = (e) => {
     e.preventDefault();
-
-    navigate("/");
+    onAddUser();
   };
-  // const [logInID, setLogInId] = useState("");
-  // const [password, setPassWord] = useState("")
-  // const [nickName, setNickName] = useState("")
-  // const [newUser, setNewUser] = useState([]);
-
-  // const onChangeId = () => {
-  //   setLogInId
-  // }
 
   return (
     <StSignInForm onSubmit={onSubmit}>
@@ -25,6 +45,7 @@ const SignInPage = () => {
         minLength={4}
         maxLength={10}
         required
+        onChange={(e) => setLogInId(e.target.value)}
       />
       <input
         type="password"
@@ -32,6 +53,7 @@ const SignInPage = () => {
         minLength={4}
         maxLength={15}
         required
+        onChange={(e) => setPassWord(e.target.value)}
       />
       <input
         type="text"
@@ -39,19 +61,23 @@ const SignInPage = () => {
         minLength={1}
         maxLength={10}
         required
+        onChange={(e) => setNickName(e.target.value)}
       />
-      <StLink to="/home">회원가입</StLink>
+      <StLink to="#" onClick={onSubmit}>
+        회원가입
+      </StLink>
       <StLink to="/">로그인</StLink>
     </StSignInForm>
   );
 };
+//온클릭에 onAddUder가 맞는지 onSubmit이 맞는지..//
 // 회원가입이 제대로 됐으면 얼럿과 함께 home.jsx이동로직 필요할듯 //
 
 export default SignInPage;
 
 // 아이디는 4~10글자로, 비밀번호는 4~15글자로, 닉네임은 1~10글자로 제한하세요.  --> 해야 함!
 
-const StSignInForm = styled.div`
+const StSignInForm = styled.form`
   max-width: 400px;
   margin: 0px auto;
   padding: 20px;
@@ -65,7 +91,7 @@ const StSignInForm = styled.div`
   }
 `;
 
-const StLink = styled(Link)`
+const StLink = styled.button`
   width: 100%;
   padding: 10px;
   background-color: rgb(0, 123, 255);
